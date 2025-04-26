f2752607be1baa7a2e680ae61f549908c86ac2a1
Ekaterina <ekaterinaduzh@gmail.com>
2025-04-26 19:33:48 +0300

    Задание

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,5 @@
-// TODO: write code here
+import Start from "./start.js";
 
-// comment this to pass build
-const unusedVariable = "variable";
-
-// for demonstration purpose only
-export default function demo(value) {
-  return `Demo: ${value}`;
-}
-
-console.log("app.js included");
+document.addEventListener("DOMContentLoaded", () => {
+  Start.init();
+});
